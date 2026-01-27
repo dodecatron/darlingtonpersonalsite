aac2e7f90d946c3300e2c509414946a5e00d1a29
dodecatron <158203954+dodecatron@users.noreply.github.com>
2026-01-27 02:08:33 -0500

    more bug fixes

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -12,7 +12,7 @@ import { Resume } from "./pages/Resume"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
