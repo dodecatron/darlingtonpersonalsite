c40173d86f663c76696e70c99ff38cf6d87a2e67
dodecatron <158203954+dodecatron@users.noreply.github.com>
2026-01-27 01:48:33 -0500

    updated config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 export default defineConfig({
-  base: "/darlingtonpersonalsite/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
